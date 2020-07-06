c8e3dffe96fbbd5fb22fb6f9353b8a080e5e2292
Simzerrr <67275466+Simzerrr@users.noreply.github.com>
2020-07-06 15:33:20 +0200

    Create vitrine.php

---
+++ ex 02/vitrine.php
@@ -0,0 +1,7 @@
+<nav class="menu">
+    <ul>
+        <a <?php if($page_title == "Accueil") { echo'class="active"';} ?> href="vitrine-accueil.php">Accueil</a>
+        <a <?php if($page_title == "Programme") { echo'class="active"';} ?> href="vitrine-progamme.php">Programme</a>
+        <a <?php if($page_title == "Contact") { echo'class="active"';} ?> href="vitrine-contacts.php">Contact</a>
+</ul>
+</nav>
