aab2ed6cdfb8aad5f3c2a349953e52e5b5b53549
Simzerrr <67275466+Simzerrr@users.noreply.github.com>
2020-07-06 15:32:55 +0200

    Create vitrine accueil.php

---
+++ ex 02/vitrine accueil.php
@@ -0,0 +1,43 @@
+<DOCTYPE html>
+<html lang="fr">
+<head> 
+    <title>Summer Code Camp</title>
+    <meta name="viewport" content="width=device=width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/vitrine.css">
+</head>    
+
+<body>
+    <header>
+        <h1> Accueil </h1>
+        <nav>
+            <ul>
+                <li><a href="vitrine-accueil.php">Accueil</a></li>
+                <li><a href="vitrine-programme.php">Programme</a></li>
+                <li><a href="vitrine-contacts.php">Contacts</a></li>
+            </ul>
+        </nav>    
+    </header>
+    <main>
+        <section>
+            <h1>Bienvenu sur votre premier site !</h1>
+            <p>
+
+            </p>
+        </section>
+        <section>
+            <h2>Article moins important</h2>
+        <p>
+
+        </p>
+       </section>
+       <section>
+           <h2>Toujours un article</h2>
+           <p>
+               
+           </p>
+       </section>     
+    </main>
+    <?php
+    include "footer.php"
+    ?>
+</body>
