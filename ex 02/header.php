4e900a0f05a08a825ee1dd63dd65f97eda83315b
Simzerrr <67275466+Simzerrr@users.noreply.github.com>
2020-07-06 15:32:33 +0200

    Create header.php

---
+++ ex 02/header.php
@@ -0,0 +1,10 @@
+<header>
+    <nav>
+        <ul>
+            <a class="accueil" href="vitrine-accueil.php"><b>Accueil</b></a>
+            <a href="vitrine-contact.php">Contacts</a>
+            <a href="vitrine-programme.php">Programme</a>
+        <ul>
+</nav>
+<hl>"Summer Code Camp" </hl>
+</header>
